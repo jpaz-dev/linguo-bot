694a0d1e830246bff0592c65ddc01bb3fd6d3be9
jpaz <jpaz@sysone.com>
2020-12-14 16:53:11 -0300

    Add cron and greeting func

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
+const cron = require("node-cron");
 
 dotenv.config();
 
 const client = new Discord.Client();
 
-const prefix = "!";
-
-client.on("message", (message) => {
-	if (message.author.bot) return;
-
-	if (!message.content.startsWith(prefix)) return;
-
-	const commandBody = message.content.slice(prefix.length);
-	const args = commandBody.split(" ");
-	const command = args.shift().toLowerCase();
-
-	if (command === "saludar") {
-		message.reply(`Hola ${message.author.username}! :)`);
+client.login(process.env.BOT_TOKEN).then(async () => {
+	const guild = await client.guilds.fetch(process.env.GUILD_ID, false, true);
+	if (!guild) {
+		console.log("Not guild found");
+		return;
 	}
 
-	console.log(message);
-});
+	const channel = guild.channels.cache.find((ch) => ch.id === process.env.CHANNEL_ID);
+	if (!channel) {
+		console.log("Not channel found");
+		return;
+	}
 
-client.login(process.env.BOT_TOKEN).then(() => console.log("Bot is up"));
+	const greeting = () => {
+		const attachement = new Discord.MessageAttachment(
+			"https://media1.tenor.com/images/28fc963091e05e1deb32e096c28f040f/tenor.gif?itemid=18184379",
+			"asuka.gif"
+		);
+		channel.send("¡Feliz Jueves!", attachement);
+	};
+
+	//  * * * * * *
+	//  | | | | | |_ day of week
+	//  | | | | |___ month
+	//  | | | |_____ day of month
+	//  | | |_______ hour
+	//  | |_________ minute
+	//  |___________ second (optional)
+	//
+	cron.schedule("0 0 9 * * 4", () => greeting(), { timezone: "America/Argentina/Buenos_Aires" });
+});
