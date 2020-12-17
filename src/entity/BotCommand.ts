40b7a554bce99b3fe40c7697d24fc5cc36ca4ba9
Jonathan Nicolas Paz <jonathannicolaspaz@outlook.com>
2020-12-17 11:37:57 -0300

    add class validator and add validation pattern to bot command entity

--- src/entity/BotCommand.ts
+++ src/entity/BotCommand.ts
@@ -1,8 +1,10 @@
+import { Matches } from "class-validator";
 import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class BotCommand extends BaseEntity {
 	@PrimaryColumn()
+	@Matches(/^[a-zA-Z0-9\-_]{3,40}$/)
 	command: string;
 
 	@Column({ name: "create_at", default: new Date() })
